c27de338b22bf2616d7769e3d74cc8b7935247e7
WIL PANNELL <wgpannel@us.ibm.com>
2018-03-31 11:32:31 -0500

    add prepush hook

--- server/node/3-unsubscribe.js
+++ server/node/3-unsubscribe.js
@@ -1,5 +1,6 @@
 // `data$` is an observable stream of 10 numbers.
 const data$ = require('./fixtures/1-data.js');
+console.log(data$);
 
 // TODO: Get the subscription and unsubscribe it after 1 second
 
--- server/node/helpers/createLoggingObserver.js
+++ server/node/helpers/createLoggingObserver.js
@@ -1,7 +1,7 @@
 module.exports = function createLoggingObserver (name) {
   return {
     next(x) { console.log(name, x); },
-    error(err) { console.error(name, error); },
+    error(err) { console.error(name, err); },
     complete() { console.info(name, 'done'); }
-  }
-}
+  };
+};
